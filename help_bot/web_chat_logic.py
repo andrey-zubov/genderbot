d69e7d4b695466e4928e0b3afd2a71f3945a0dc3
bequite <valentin8dev@gmail.com>
2019-10-07 19:38:53 +0300

    upd: chat.html

--- help_bot/web_chat_logic.py
+++ help_bot/web_chat_logic.py
@@ -1,13 +1,32 @@
+import logging
 from time import perf_counter
+
+from help_bot.models import NeedHelp, StartMessage
 
 
 def chat_req_get(request):
     """ TBA """
     time_0 = perf_counter()
 
-    ui = request.GET['us_in']
-    data = "%s%s" % (ui, 'test_bot_test_bot')
+    if any(request.GET.values()):
+        ui = request.GET['us_in']
 
-    print("chat_req_get(), data: %s" % data)
-    print("\tTIME chat_req_get() = %s\n" % (perf_counter() - time_0))
-    return data
+        data = start_chat()
+
+        print("chat_req_get(), data: %s" % data)
+        print("\tTIME chat_req_get() = %s\n" % (perf_counter() - time_0))
+        return data
+    else:
+        logging.error("request.GET is empty!")
+        return start_chat()
+
+
+def start_chat():
+    """ TBA """
+    root_nodes = NeedHelp.objects.root_nodes()
+    btn_text = [i.user_input for i in root_nodes]
+    print("btn_text: %s" % btn_text)
+    btn = '|'.join(btn_text)
+    text = StartMessage.objects.get().text
+
+    return "%s#%s" % (btn, text)
