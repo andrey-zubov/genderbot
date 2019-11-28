e2cd49372086fe6b063e2315d5bd39335cf5b2e2
bequite <valentin8dev@gmail.com>
2019-11-28 18:31:02 +0300

    set_all_statistic_to_zero

--- help_bot/loger_set_up.py
+++ help_bot/loger_set_up.py
@@ -24,13 +24,13 @@ def logger_telegram():
 
 def logger_web_chat():
     logger = logging.getLogger('web_chat_bot')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.WARNING)
     # create file handler which logs even debug messages
     fh = logging.FileHandler('web_chat_bot.log')
-    fh.setLevel(logging.INFO)
+    fh.setLevel(logging.WARNING)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.ERROR)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
@@ -44,13 +44,13 @@ def logger_web_chat():
 
 def logger_utility():
     logger = logging.getLogger('app_utilities')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.WARNING)
     # create file handler which logs even debug messages
     fh = logging.FileHandler('app_utilities.log')
-    fh.setLevel(logging.INFO)
+    fh.setLevel(logging.WARNING)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.ERROR)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
